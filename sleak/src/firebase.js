f167ed74e8c7106e7825421e8effd98f96adbdb4
Greg Hayes <gthelight@gmail.com>
2021-03-08 16:51:46 -0500

    initial CRA with redux

---
+++ sleak/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlSBF2R03oQApJ5aazgVQanDp01oM_j_8",
+  authDomain: "sleak-1acdd.firebaseapp.com",
+  projectId: "sleak-1acdd",
+  storageBucket: "sleak-1acdd.appspot.com",
+  messagingSenderId: "219272970695",
+  appId: "1:219272970695:web:f8184c8741226d566059e2",
+};
