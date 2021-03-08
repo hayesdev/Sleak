0050196cc6f1a9e1f4c3c9eab8551b934afa9215
Greg Hayes <gthelight@gmail.com>
2021-03-08 18:22:23 -0500

    completed header component

--- sleak/src/components/Header.js
+++ sleak/src/components/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
   return (
     <HeaderContainer>
-      {/* header left */}
+      {/* Header Left */}
       <HeaderLeft>
         <HeaderAvatar />
+        <AccessTimeIcon />
       </HeaderLeft>
 
-      {/* header middle */}
+      {/* Header Search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search Sleak" />
+      </HeaderSearch>
 
-      {/* header right */}
+      {/* Header Right */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 }
@@ -20,9 +31,64 @@ function Header() {
 export default Header;
 
 const HeaderContainer = styled.div`
-  color: red;
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--sleak-color);
+  color: white;
 `;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
-const HeaderAvatar = styled(Avatar)``;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  display: flex;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
