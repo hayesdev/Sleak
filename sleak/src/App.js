cba6cdb0d19bef806ba5034c6720b3174a0f079a
Greg Hayes <gthelight@gmail.com>
2021-03-08 19:12:00 -0500

    building siderbar, basic styling done

--- sleak/src/App.js
+++ sleak/src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header.js";
+import Sidebar from "./components/Sidebar.js";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* This will be Chat component */}
+              </Route>
+            </Switch>
+          </AppBody>
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
         </>
       </Router>
     </div>
@@ -22,3 +28,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
