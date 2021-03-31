e656c116dca652f54a86139b4ac282c56b115330
Greg Hayes <gthelight@gmail.com>
2021-03-31 00:01:37 -0400

    added env file

--- sleak/src/firebase.js
+++ sleak/src/firebase.js
@@ -1,12 +1,21 @@
 import firebase from "firebase";
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDlSBF2R03oQApJ5aazgVQanDp01oM_j_8",
-  authDomain: "sleak-1acdd.firebaseapp.com",
-  projectId: "sleak-1acdd",
-  storageBucket: "sleak-1acdd.appspot.com",
-  messagingSenderId: "219272970695",
-  appId: "1:219272970695:web:f8184c8741226d566059e2",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
