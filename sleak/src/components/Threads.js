1913e85adb02d97ca085874fdde63c5a7f1a4b87
Greg Hayes <gthelight@gmail.com>
2021-04-07 18:17:23 -0400

    added Threads component

---
+++ sleak/src/components/Threads.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Threads() {
+  return (
+    <div>
+      <h3>This is where you can start a new thread in Sleak :)</h3>
+      <p>
+        <a href="#">Click here</a> to start a thread!
+      </p>
+    </div>
+  );
+}
+
+export default Threads;
